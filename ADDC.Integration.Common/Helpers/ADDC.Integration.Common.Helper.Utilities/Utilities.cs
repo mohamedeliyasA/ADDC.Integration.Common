cc8d8ef0f12b79869418916dabffd616c1dfc26c
mohamedeliyasA <49101626+mohamedeliyasA@users.noreply.github.com>
2022-03-07 21:05:11 +0100

    First Commit

---
+++ ADDC.Integration.Common/Helpers/ADDC.Integration.Common.Helper.Utilities/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OracleClient;
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+namespace ADDC.Integration.Common.Helper.Utilities
+{
+    [Serializable]
+    public class Utilities
+    {
+       
+    }
+}
